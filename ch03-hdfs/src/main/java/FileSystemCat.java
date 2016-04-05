ea4206750bb4076242a59cc23593c79bb12e2672
Alin Posorovaschi <alin.posorovaschi@e-spres-oh.com>
2016-04-05 16:30:09 +0300

    Use Java 7 try-finally

--- ch03-hdfs/src/main/java/FileSystemCat.java
+++ ch03-hdfs/src/main/java/FileSystemCat.java
@@ -14,12 +14,8 @@ public class FileSystemCat {
     String uri = args[0];
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(URI.create(uri), conf);
-    InputStream in = null;
-    try {
-      in = fs.open(new Path(uri));
+    try (InputStream in = fs.open(new Path(uri))) {
       IOUtils.copyBytes(in, System.out, 4096, false);
-    } finally {
-      IOUtils.closeStream(in);
     }
   }
 }
