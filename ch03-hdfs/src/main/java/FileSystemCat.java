2223088da8c36417d4493c134bad97be7cecb7cd
Alin Posorovaschi <alinposho@yahoo.com>
2016-04-07 20:08:52 +0300

    Make the call more explicit

--- ch03-hdfs/src/main/java/FileSystemCat.java
+++ ch03-hdfs/src/main/java/FileSystemCat.java
@@ -10,12 +10,14 @@ import org.apache.hadoop.io.IOUtils;
 // vv FileSystemCat
 public class FileSystemCat {
 
+  private static final boolean dontClose = false;
+
   public static void main(String[] args) throws Exception {
     String uri = args[0];
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(URI.create(uri), conf);
     try (InputStream in = fs.open(new Path(uri))) {
-      IOUtils.copyBytes(in, System.out, 4096, false);
+      IOUtils.copyBytes(in, System.out, 4096, dontClose);
     }
   }
 }
